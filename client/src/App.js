a011d55804daf439c609158c2f59b3ef771794a3
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2020-02-29 23:46:11 -0500

    inputs to add user

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const localhost = 'http://localhost:4000/api/v1/users'
+const api = 'https://api-dot-mernongae.appspot.com/api/v1/users'
 
 function App() {
   const [users, setUsers] = useState([])
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastName] = useState()
 
-  const fetchUsers = () => {
-    fetch('https://api-dot-mernongae.appspot.com/api/users')
-      .then(res => res.json())
-      .then(users => setUsers(users))
+  const fetchUsers = async () => {
+    const res = await fetch(localhost)
+    const data = await res.json()
+    setUsers(data.data)
   }
+
+  const addUser = async (e) => {
+    e.preventDefault()
+    const res = await fetch(localhost, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        firstName, lastName
+      })
+    })
+    const data = await res.json()
+    console.log('data', data)
+    await fetchUsers()
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={fetchUsers}>Get Users</button>
+        <form onSubmit={addUser}>
+          <input
+            type='text'
+            value={firstName}
+            placeholder='first name'
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type='text'
+            value={lastName}
+            placeholder='last name'
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <button>Add User</button>
+        </form>
         <h3>Users</h3>
         <ul>
           {
             users.map(user => (
-              <li>Name: {user?.name}, Age: {user?.age}</li>
+              <li key={user._id}>{user.firstName} {user.lastName}</li>
             ))
           }
         </ul>
